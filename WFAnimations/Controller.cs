77993cbca207900551b52b76c8e97b021a62d667
aminmirzaei <aminmirzaei.og@gmail.com>
2024-10-11 13:26:51 +0330

    - Create Frame Property In Controller Class

--- WFAnimations/Controller.cs
+++ WFAnimations/Controller.cs
@@ -13,6 +13,8 @@ namespace WFAnimations
     public class Controller
     {
         protected Bitmap BgBmp { get { return (DoubleBitmap as IFakeControl).BgBmp; } set { (DoubleBitmap as IFakeControl).BgBmp = value; } }
+        public Bitmap Frame { get { return (DoubleBitmap as IFakeControl).Frame; } set { (DoubleBitmap as IFakeControl).Frame = value; } }
+
 
     }
 }
