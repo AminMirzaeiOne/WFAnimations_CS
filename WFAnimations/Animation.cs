0134887d71704cb368f738cba318e0dce47d09db
aminmirzaei <aminmirzaei.og@gmail.com>
2024-10-11 12:43:46 +0330

    - Create Leaf Property In Animation Class

--- WFAnimations/Animation.cs
+++ WFAnimations/Animation.cs
@@ -82,6 +82,7 @@ namespace WFAnimations
         public static Animation HorizSlideAndRotate { get { return new Animation { SlideCoeff = new PointF(1, 0), RotateCoeff = 0.3f, RotateLimit = 0.2f, Padding = new Padding(50, 50, 50, 50) }; } }
         public static Animation ScaleAndHorizSlide { get { return new Animation { ScaleCoeff = new PointF(1, 1), SlideCoeff = new PointF(1, 0), Padding = new Padding(30, 0, 0, 0) }; } }
         public static Animation Transparent { get { return new Animation { TransparencyCoeff = 1 }; } }
+        public static Animation Leaf { get { return new Animation { LeafCoeff = 1 }; } }
 
 
     }
