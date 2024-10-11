2ea7225148cd550a50f281bc0ccfacb0f090d1ad
aminmirzaei <aminmirzaei.og@gmail.com>
2024-10-11 17:39:55 +0330

    - Create PointFConverter Empty Constractor Class

--- WFAnimations/PointFConverter.cs
+++ WFAnimations/PointFConverter.cs
@@ -1,13 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace WFAnimations
 {
-    public class PointFConverter
+    public class PointFConverter : ExpandableObjectConverter
     {
-
+        /// <summary>
+        /// Creates a new instance of PointFConverter
+        /// </summary>
+        public PointFConverter()
+        {
+        }
     }
 }
