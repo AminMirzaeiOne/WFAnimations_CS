0a3a2b77a25bfd1a5446fdee26e8cbebcd4f3d06
aminmirzaei <aminmirzaei.og@gmail.com>
2024-10-11 17:43:30 +0330

    - Create DoScale Static Method

--- WFAnimations/TransfromHelper.cs
+++ WFAnimations/TransfromHelper.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +12,18 @@ namespace WFAnimations
         const int bytesPerPixel = 4;
         static Random rnd = new Random();
 
+        public static void DoScale(TransfromNeededEventArg e, Animation animation)
+        {
+            var rect = e.ClientRectangle;
+            var center = new PointF(rect.Left + rect.Width / 2, rect.Top + rect.Height / 2);
+            e.Matrix.Translate(center.X, center.Y);
+            var kx = 1f - animation.ScaleCoeff.X * e.CurrentTime;
+            var ky = 1f - animation.ScaleCoeff.X * e.CurrentTime;
+            if (Math.Abs(kx) <= 0.001f) kx = 0.001f;
+            if (Math.Abs(ky) <= 0.001f) ky = 0.001f;
+            e.Matrix.Scale(kx, ky);
+            e.Matrix.Translate(-center.X, -center.Y);
+        }
 
     }
 }
