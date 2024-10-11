3fb8c7db7944830903ef2ba6d1a497c292399ee0
aminmirzaei <aminmirzaei.og@gmail.com>
2024-10-11 15:39:47 +0330

    - Create DecoratedControl Property

--- WFAnimations/DecorationControl.cs
+++ WFAnimations/DecorationControl.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace WFAnimations
 {
     public class DecorationControl : System.Windows.Forms.UserControl
     {
         public DecorationType DecorationType { get; set; }
+        public Control DecoratedControl { get; set; }
+
 
     }
 }
