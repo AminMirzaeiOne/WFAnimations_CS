e19b4591d470d1df528f95c233f53982202af5cc
aminmirzaei <aminmirzaei.og@gmail.com>
2024-10-11 13:23:54 +0330

    - Create NonLinearTransfromNeededEventArg Class In WFAnimations Namespace

--- WFAnimations/Animator.cs
+++ WFAnimations/Animator.cs
@@ -759,4 +759,24 @@ namespace WFAnimations
         public bool UseDefaultMatrix { get; set; }
     }
 
+    public class NonLinearTransfromNeededEventArg : EventArgs
+    {
+        public float CurrentTime { get; internal set; }
+
+        public Rectangle ClientRectangle { get; internal set; }
+        public byte[] Pixels { get; internal set; }
+        public int Stride { get; internal set; }
+
+        public Rectangle SourceClientRectangle { get; internal set; }
+        public byte[] SourcePixels { get; internal set; }
+        public int SourceStride { get; set; }
+
+        public Animation Animation { get; set; }
+        public Control Control { get; internal set; }
+        public AnimateMode Mode { get; internal set; }
+        public bool UseDefaultTransform { get; set; }
+    }
+
+
+
 }
