de74ed2ba82aa0aad0d84a731bf9cd49efd2b9fc
aminmirzaei <aminmirzaei.og@gmail.com>
2024-10-11 11:34:29 +0330

    - Create RotateCoeff Property In Animation Class

--- WFAnimations/Animation.cs
+++ WFAnimations/Animation.cs
@@ -7,10 +7,18 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace WFAnimations
 {
+    /// <summary>
+    /// Settings of animation
+    /// </summary>
     public class Animation
     {
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Visible), EditorBrowsable(EditorBrowsableState.Advanced), TypeConverter(typeof(PointFConverter))]
+        public PointF SlideCoeff { get; set; }
+
+        public float RotateCoeff { get; set; }
 
     }
 }
