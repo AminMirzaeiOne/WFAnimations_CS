81de171e1afa5b361d9ff78d7a67c232ea9136f6
aminmirzaei <aminmirzaei.og@gmail.com>
2024-10-11 12:49:50 +0330

    - Add ProvideProperty To Animator Component

--- WFAnimations/Animator.cs
+++ WFAnimations/Animator.cs
@@ -4,9 +4,14 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace WFAnimations
 {
+    /// <summary>
+    /// Animation manager
+    /// </summary>
+    [ProvideProperty("Decoration", typeof(System.Windows.Forms.Control))]
     public class Animator : Component, IExtenderProvider
     {
 
