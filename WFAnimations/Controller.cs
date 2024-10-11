94f31b6c914c10e975c0ef887cfce2c27950ace1
aminmirzaei <aminmirzaei.og@gmail.com>
2024-10-11 13:26:22 +0330

    - Create BgBmp Property In Controller Class

--- WFAnimations/Controller.cs
+++ WFAnimations/Controller.cs
@@ -1,13 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace WFAnimations
 {
+    /// <summary>
+    /// DoubleBitmap displays animation
+    /// </summary>
     public class Controller
     {
+        protected Bitmap BgBmp { get { return (DoubleBitmap as IFakeControl).BgBmp; } set { (DoubleBitmap as IFakeControl).BgBmp = value; } }
 
     }
 }
