a764848f225c42571b4cbb1c2529e35bc791e4be
aminmirzaei <aminmirzaei.og@gmail.com>
2024-10-11 17:44:57 +0330

    - Create DoMosaic Static Method

--- WFAnimations/TransfromHelper.cs
+++ WFAnimations/TransfromHelper.cs
@@ -61,5 +61,68 @@ namespace WFAnimations
                 }
         }
 
+        public static void DoMosaic(NonLinearTransfromNeededEventArg e, Animation animation, ref Point[] buffer, ref byte[] pixelsBuffer)
+        {
+            if (animation.MosaicCoeff == PointF.Empty || animation.MosaicSize == 0)
+                return;
+
+            var pixels = e.Pixels;
+            System.Int32 sx = e.ClientRectangle.Width;
+            System.Int32 sy = e.ClientRectangle.Height;
+            System.Int32 s = e.Stride;
+            System.Single a = e.CurrentTime;
+            System.Int32 count = pixels.Length;
+            System.Single opacity = 1 - e.CurrentTime;
+            if (opacity < 0f) opacity = 0f;
+            if (opacity > 1f) opacity = 1f;
+            System.Single mkx = animation.MosaicCoeff.X;
+            System.Single mky = animation.MosaicCoeff.Y;
+
+            if (buffer == null)
+            {
+                buffer = new Point[pixels.Length];
+                for (int i = 0; i < pixels.Length; i++)
+                    buffer[i] = new Point((int)(mkx * (rnd.NextDouble() - 0.5)), (int)(mky * (rnd.NextDouble() - 0.5)));
+            }
+
+            if (pixelsBuffer == null)
+                pixelsBuffer = (byte[])pixels.Clone();
+
+
+            for (int i = 0; i < count; i += bytesPerPixel)
+            {
+                pixels[i + 0] = 255;
+                pixels[i + 1] = 255;
+                pixels[i + 2] = 255;
+                pixels[i + 3] = 0;
+            }
+
+            var ms = animation.MosaicSize;
+            var msx = animation.MosaicShift.X;
+            var msy = animation.MosaicShift.Y;
+
+            for (int y = 0; y < sy; y++)
+                for (int x = 0; x < sx; x++)
+                {
+                    int yi = (y / ms);
+                    int xi = (x / ms);
+                    int i = y * s + x * bytesPerPixel;
+                    int j = yi * s + xi * bytesPerPixel;
+
+                    var newX = x + (int)(a * (buffer[j].X + xi * msx));
+                    var newY = y + (int)(a * (buffer[j].Y + yi * msy));
+
+                    if (newX >= 0 && newX < sx)
+                        if (newY >= 0 && newY < sy)
+                        {
+                            int newI = newY * s + newX * bytesPerPixel;
+                            pixels[newI + 0] = pixelsBuffer[i + 0];
+                            pixels[newI + 1] = pixelsBuffer[i + 1];
+                            pixels[newI + 2] = pixelsBuffer[i + 2];
+                            pixels[newI + 3] = (byte)(pixelsBuffer[i + 3] * opacity);
+                        }
+                }
+        }
+
     }
 }
