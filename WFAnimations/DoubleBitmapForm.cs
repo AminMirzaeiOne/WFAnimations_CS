3df196099368ae8575ab0ae35eef75ab5567fd77
aminmirzaei <aminmirzaei.og@gmail.com>
2024-10-11 17:29:55 +0330

    - Create TransfromNeeded Event

--- WFAnimations/DoubleBitmapForm.cs
+++ WFAnimations/DoubleBitmapForm.cs
@@ -15,6 +15,9 @@ namespace WFAnimations
         Bitmap bgBmp;
         Bitmap frame;
 
+        public event EventHandler<TransfromNeededEventArg> TransfromNeeded;
+
+
         public DoubleBitmapForm()
         {
             InitializeComponent();
